9bcb456e905636d2f106134d45d3611e1111405d
jking11 <58608326+strontiumdesign@users.noreply.github.com>
2024-01-23 00:00:58 +0000

    UE5 project created

---
+++ UE5 Project/Source/Cattlezone/CattlezoneGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "CattlezoneGameModeBase.h"
+
---
+++ UE5 Project/Source/Cattlezone/CattlezoneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CattlezoneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CATTLEZONE_API ACattlezoneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
